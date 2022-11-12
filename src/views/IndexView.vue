f621a64a0906e5e68fa9f6e4f061f6fd5ec46838
CMartinez-BB <charli.rey.misterio@gmail.com>
2022-11-12 14:35:52 -0600

    first

---
+++ src/views/IndexView.vue
@@ -0,0 +1,22 @@
+<template>
+  <header class="header-2">
+    <div class="page-header min-vh-75" style="background-image: url('/img/bg0.jpg')" loading="lazy">
+      <span class="mask bg-gradient-primary opacity-4"></span>
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-7 text-center mx-auto">
+            <h1 class="text-white pt-3 mt-n5">AgroBlock</h1>
+            <p class="lead text-white mt-3 px-5">Seguridad a la mano</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </header>
+  
+</template>
+<script>
+export default {
+  name:'index-component'
+}
+</script>
+
